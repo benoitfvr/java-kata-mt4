4955395ac3bdc3cc17d21e317467ce447c6362f7
Benoît Favrie <benoit14.favrie@gmail.com>
2024-06-27 15:21:35 +0200

    feat: add values and symbols

---
+++ src/main/java/fr/hetic/RomanNumerals.java
@@ -0,0 +1,11 @@
+package fr.hetic;
+
+public class RomanNumerals {
+
+    private static final int[] values = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+    private static final String[] symbols = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+
+    public static String toRoman(int number) {
+        return "";
+    }
+}
