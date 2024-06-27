0ec6f4007d99a6f401ef3ccfa253663f4f7dfb4d
Benoît Favrie <benoit14.favrie@gmail.com>
2024-06-27 15:53:34 +0200

    feat: integrate all tests

--- src/test/java/fr/hetic/RomanNumeralsTest.java
+++ src/test/java/fr/hetic/RomanNumeralsTest.java
@@ -8,5 +8,39 @@ public class RomanNumeralsTest {
     @Test
     public void testToRoman_singleDigits() {
         assertThat(RomanNumerals.toRoman(1)).isEqualTo("I");
+        assertThat(RomanNumerals.toRoman(2)).isEqualTo("II");
+        assertThat(RomanNumerals.toRoman(5)).isEqualTo("V");
+        assertThat(RomanNumerals.toRoman(7)).isEqualTo("VII");
+        assertThat(RomanNumerals.toRoman(9)).isEqualTo("IX");
+    }
+
+    @Test
+    public void testToRoman_doubleDigits() {
+        assertThat(RomanNumerals.toRoman(10)).isEqualTo("X");
+        assertThat(RomanNumerals.toRoman(40)).isEqualTo("XL");
+        assertThat(RomanNumerals.toRoman(50)).isEqualTo("L");
+        assertThat(RomanNumerals.toRoman(80)).isEqualTo("LXXX");
+        assertThat(RomanNumerals.toRoman(90)).isEqualTo("XC");
+    }
+
+    @Test
+    public void testToRoman_tripleDigits() {
+        assertThat(RomanNumerals.toRoman(100)).isEqualTo("C");
+        assertThat(RomanNumerals.toRoman(400)).isEqualTo("CD");
+        assertThat(RomanNumerals.toRoman(500)).isEqualTo("D");
+        assertThat(RomanNumerals.toRoman(800)).isEqualTo("DCCC");
+        assertThat(RomanNumerals.toRoman(900)).isEqualTo("CM");
+    }
+
+    @Test
+    public void testToRoman_quadrupleDigits() {
+        assertThat(RomanNumerals.toRoman(1000)).isEqualTo("M");
+        assertThat(RomanNumerals.toRoman(3000)).isEqualTo("MMM");
+    }
+
+    @Test
+    public void testToRoman_variousNumbers() {
+        assertThat(RomanNumerals.toRoman(1987)).isEqualTo("MCMLXXXVII");
+        assertThat(RomanNumerals.toRoman(2024)).isEqualTo("MMXXIV");
     }
 }
